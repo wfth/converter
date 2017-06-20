3e8ebeacd97b2cf7f8d9cdca79259b02a949666b
ijwilliams <isaac@thewilliams.ws>
2017-06-20 14:06:23 -0400

    Preserve order from Collector (closes #2)

--- lib/tasks/convert.ex
+++ lib/tasks/convert.ex
@@ -14,14 +14,14 @@ defmodule Mix.Tasks.Converter.Convert do
   end
 
   def convert do
-    co_sermon_series = Converter.CollectorRepo.all(from(ss in "sermon_series", select: {ss.id, ss.title, ss.description, ss.passages}))
+    co_sermon_series = Converter.CollectorRepo.all(from(ss in "sermon_series", select: {ss.id, ss.title, ss.description, ss.passages}, order_by: ss.id))
     insert(co_sermon_series)
   end
 
   def insert([]), do: nil
   def insert([{co_id, title, description, passages} | tail]) do
     wo_sermon_series = add_sermon_series_to_wo_db({title, description, passages})
-    co_sermons = Converter.CollectorRepo.all(from(s in "sermons", select: {s.title, s.description, s.passage}, where: s.sermon_series_id == ^co_id))
+    co_sermons = Converter.CollectorRepo.all(from(s in "sermons", select: {s.title, s.description, s.passage}, where: s.sermon_series_id == ^co_id, order_by: s.id))
     Enum.map(co_sermons, fn(s) -> add_sermon_to_wo_db(s, wo_sermon_series.id) end)
 
     insert(tail)
